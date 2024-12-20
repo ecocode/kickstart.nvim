ca99a6be371994975b3aee4550a7407950f9f719
erik colson <eco@ecocode.net>
2024-12-20 10:48:08 +0100

    remember last location in file

--- init.lua
+++ init.lua
@@ -210,6 +210,24 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- added by EC to remember last cursor position of files.
+-- there is a "lastplace" plugin that enhances this with many options.
+vim.api.nvim_create_autocmd('BufRead', {
+  callback = function(opts)
+    vim.api.nvim_create_autocmd('BufWinEnter', {
+      once = true,
+      buffer = opts.buf,
+      callback = function()
+        local ft = vim.bo[opts.buf].filetype
+        local last_known_line = vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
+        if not (ft:match 'commit' and ft:match 'rebase') and last_known_line > 1 and last_known_line <= vim.api.nvim_buf_line_count(opts.buf) then
+          vim.api.nvim_feedkeys([[g`"]], 'nx', false)
+        end
+      end,
+    })
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
